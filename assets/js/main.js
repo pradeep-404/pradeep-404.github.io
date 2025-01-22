1d8da1351477d010de192ea37498c9300d0feaea
PRADEEP RANWA <pradeepranva419@gmail.com>
2025-01-22 16:52:46 +0530

    last todays commit
    i do some improvement  in this code

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Data Ana","top","Developer"],
+  strings : ["Data Analytics","python Developer","Web Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
